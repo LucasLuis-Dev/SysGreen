b24073b0b8077dac1d765b7bd9458c5b954149a7
Lucas Luis <lucasluisouza@gmail.com>
2023-04-14 00:42:25 -0300

    add informations and add effects

--- main.js
+++ main.js
@@ -1,54 +1,11 @@
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-      this.handleLinkClick = this.handleLinkClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    handleLinkClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-      this.navLinks.forEach((link) => {
-        link.addEventListener("click", this.handleClick);
-      });
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100) {
+    navbar.classList.add('white');
+  } else {
+    navbar.classList.remove('white');
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
-  
+});
